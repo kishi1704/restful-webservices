7451a9054213681935e2784bde6afbfcaa4c7ca6
Truong Van Tien <98truongtien@gmail.com>
2023-11-01 16:12:38 +0700

    handle ResourceNotFoundException exception

---
+++ src/main/java/com/restapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.restapi.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+																		WebRequest webRequest) {
+		ErrorDetails errorDetails = new ErrorDetails(
+				LocalDateTime.now(),
+				exception.getMessage(),
+				webRequest.getDescription(false),
+				"USER_NOT_FOUND"
+		);
+		
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+}
