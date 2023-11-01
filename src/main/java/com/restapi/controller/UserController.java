decf2057a57357dd5b447103b8c84bf1e0fe5d72
Truong Van Tien <98truongtien@gmail.com>
2023-11-01 11:12:30 +0700

    build update User REST API

--- src/main/java/com/restapi/controller/UserController.java
+++ src/main/java/com/restapi/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,14 @@ public class UserController {
 		List<User> users = userService.getAllUsers();
 		
 		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	// build update User REST API
+	@PutMapping("/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable("id") Long userId, @RequestBody User user) {
+		user.setId(userId);
+		User updatedUser = userService.updateUser(user);
+		
+		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
 	}
 }
