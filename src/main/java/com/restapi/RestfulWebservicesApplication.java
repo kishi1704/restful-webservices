41bd852b54dcd4fb50afbd851ec9660662c418b4
Truong Van Tien <98truongtien@gmail.com>
2023-11-02 11:10:22 +0700

    define General API Information using Annotations

--- src/main/java/com/restapi/RestfulWebservicesApplication.java
+++ src/main/java/com/restapi/RestfulWebservicesApplication.java
@@ -5,7 +5,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Tien Truong",
+						email = "98truongtien@gmail.com",
+						url = "https://github.com/kishi1704"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = ""
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation",
+				url = ""
+		)	
+)
 public class RestfulWebservicesApplication {
 
 	public static void main(String[] args) {
